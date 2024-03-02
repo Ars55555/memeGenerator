eb8186c127b379f5063ce4b93f84f08697ad194f
Ars55555 <108430044+Ars55555@users.noreply.github.com>
2024-03-03 02:03:00 +0530

    memeGenerator

---
+++ src/pages/Edit.jsx
@@ -0,0 +1,28 @@
+import React,{useState,createRef} from "react";
+import { Button } from "react-bootstrap";
+import { useSearchParams } from "react-router-dom";
+import Text from "../components/Text";
+import { exportComponentAsJPEG } from "react-component-export-image";
+import "./abcd.css";
+
+const EditPage = () =>{
+    const [params]=useSearchParams();
+    // console.log(params.get("url"));
+    const [count,setCount] = useState(0);
+    const memeRef = createRef();
+
+    const addText = () =>{
+            setCount(count+1);
+    };
+    return(  
+    <div >
+        <div ref={memeRef} className="meme mt-5 mb-5 "> 
+            <img src={params.get("url")} width="250px"/>
+            {Array(count).fill(0).map((e)=>(<Text/>))}
+        </div>
+    <Button onClick={addText}>Add Text</Button>
+    <Button variant="success" onClick={(e)=> exportComponentAsJPEG(memeRef)} >Save</Button>
+    </div>
+    )
+}
+export default EditPage;
